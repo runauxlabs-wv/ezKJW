ab81226dc11bc371a384eb910c1842b819af1670
zziwon <zhiyuan2021@naver.com>
2022-02-23 15:41:24 +0900

    수정 및 파일 제거

--- arounz/js/edu.js
+++ arounz/js/edu.js
@@ -53,40 +53,53 @@ window.onload = function () {
 
 
 
-    var tabButtons = document.querySelectorAll('.tabButton'); // 탭버튼 선언
-    // 해당 클래스명을 가지고 있는 모든 dom을 유사배열로 가져온다. 진짜 배열이 아니어서 .forEach()같은 함수를 쓸 수 없다.
-    // for문은 사용가능하나(인터넷에 돌아다니는 탭 자료들은 for문으로 제작됨)
-    // .call()을 이용하여 .forEach()를 사용할 수 있다. Array.prototype.forEach.call()로 사용한다.
+    // var tabButtons = document.querySelectorAll('.tabButton'); // 탭버튼 선언
+    // // 해당 클래스명을 가지고 있는 모든 dom을 유사배열로 가져온다. 진짜 배열이 아니어서 .forEach()같은 함수를 쓸 수 없다.
+    // // for문은 사용가능하나(인터넷에 돌아다니는 탭 자료들은 for문으로 제작됨)
+    // // .call()을 이용하여 .forEach()를 사용할 수 있다. Array.prototype.forEach.call()로 사용한다.
 
-    // Array.prototype.forEach.call(tabButtons, (eachButton) => { // 탭버튼에게 각각 적용한다 // ie에서는 화살표 함수 X
-    Array.prototype.forEach.call(tabButtons, function (eachButton) { // 탭버튼에게 각각 적용한다
-        eachButton.addEventListener('click', function () { //각 버튼을 클릭하면
+    // // Array.prototype.forEach.call(tabButtons, (eachButton) => { // 탭버튼에게 각각 적용한다 // ie에서는 화살표 함수 X
+    // Array.prototype.forEach.call(tabButtons, function (eachButton) { // 탭버튼에게 각각 적용한다
+    //     eachButton.addEventListener('click', function () { //각 버튼을 클릭하면
 
-            var tabBoxs = document.querySelectorAll('.tabBox'); // 탭박스 선언
-            var tabNum = this.getAttribute('data-num'); // 내가 누른 탭버튼에서 data-num 값 추출
+    //         var tabBoxs = document.querySelectorAll('.tabBox'); // 탭박스 선언
+    //         var tabNum = this.getAttribute('data-num'); // 내가 누른 탭버튼에서 data-num 값 추출
 
-            for (var i = 0; i < tabBoxs.length; i++) {
-                tabBoxs[i].classList.remove("on");
-                tabButtons[i].classList.remove("on");
-            }
-            tabBoxs[tabNum].classList.add("on"); // 내가 누른 탭버튼에서 data-num 값을 인덱스 번호/아이템 번호로 갖는 탭박스에 class on추가
-            // tabBoxs.item(tabNum).classList.add("on");
-            this.classList.add("on");
-            // if (!eachButton.classList.contains('on')) {
-            //     // .classList.contains 지정한 클래스가 있는지 확인한다.
-            //     // 내가 누른 탭버튼에서 class on이 없으면 추가해라
-            //     eachButton.classList.add("on");
-            // }
-            customizedEdu();
-            designSilde();
-            menuSilde();
-            if (iw < 769) {
-                CSslide();
-            };
-        });
+    //         for (var i = 0; i < tabBoxs.length; i++) {
+    //             tabBoxs[i].classList.remove("on");
+    //             tabButtons[i].classList.remove("on");
+    //         }
+    //         tabBoxs[tabNum].classList.add("on"); // 내가 누른 탭버튼에서 data-num 값을 인덱스 번호/아이템 번호로 갖는 탭박스에 class on추가
+    //         // tabBoxs.item(tabNum).classList.add("on");
+    //         this.classList.add("on");
+    //         // if (!eachButton.classList.contains('on')) {
+    //         //     // .classList.contains 지정한 클래스가 있는지 확인한다.
+    //         //     // 내가 누른 탭버튼에서 class on이 없으면 추가해라
+    //         //     eachButton.classList.add("on");
+    //         // }
+    //         customizedEdu();
+    //         designSilde();
+    //         menuSilde();
+    //         if (iw < 769) {
+    //             CSslide();
+    //         };
+    //     });
+    // });     //  ---------------------> .botton div.tabButton 에  data-num="0 , .... , data-num="6" 추가 필요
+
+
+
+
+    // data-id 와 id로 탭 만들기
+    $(".botton > div.tabButton").click(function () {
+        $(this).addClass('on').siblings().removeClass('on'); //.asideBox > li 중 클릭한 것만 선택
+        $("#" + $(this).data('id')).addClass('on').siblings().removeClass('on'); //.asideBox > li의 데이터 아이디랑 같은 아이디 찾아주기
+        customizedEdu();
+        designSilde();
+        menuSilde();
+        if (iw < 769) {
+            CSslide();
+        };
     });
-
-
 
 
 
@@ -100,6 +113,7 @@ window.onload = function () {
             observer: true,
             observeParents: true,
             //탭안의 스와이퍼 갱신
+            simulateTouch: false, // 드래깅 X
             slidesPerView: 4,
             spaceBetween: 20,
             breakpoints: {
@@ -116,7 +130,7 @@ window.onload = function () {
 
         });
     };
-    // customizedEdu();
+    customizedEdu();
 
 
 
@@ -124,17 +138,44 @@ window.onload = function () {
 
 
     function designSilde() {
-        new Swiper(".designSilde", {
-            scrollbar: {
-                el: ".swiper-scrollbar",
-                // hide: true,
-            },
-            observer: true,
-            observeParents: true,
-            //탭안의 스와이퍼 갱신
-        });
+        if ($('html').hasClass('is-ie')) {  //ie 에서만 적용
+            if ($('html').hasClass('ie11')) { //ie11 적용
+                new Swiper(".designSilde", {
+                    scrollbar: {
+                        el: ".swiper-scrollbar",
+                        // draggable: true,
+                        // hide: true,
+                    },
+                    observer: true,
+                    observeParents: true,
+                    //탭안의 스와이퍼 갱신
+                });
+            } else {  // ie11 제외 ie적용
+                new Swiper(".designSilde", {
+                    scrollbar: {
+                        el: ".swiper-scrollbar",
+                        draggable: true,
+                        // hide: true,
+                    },
+                    simulateTouch: false, // 드래깅 X
+                });
+            }
+            
+        } else {
+            new Swiper(".designSilde", {
+                scrollbar: {
+                    el: ".swiper-scrollbar",
+                    // draggable: true,
+                    // hide: true,
+                },
+                observer: true,
+                observeParents: true,
+                //탭안의 스와이퍼 갱신
+            });
+        }
+        
     };
-    // designSilde();
+    designSilde();
 
 
 
@@ -149,6 +190,7 @@ window.onload = function () {
             observer: true,
             observeParents: true,
             //탭안의 스와이퍼 갱신
+            simulateTouch: false, // 드래깅 X
             slidesPerView: 3.2,
             spaceBetween: 28,
             breakpoints: {
@@ -159,7 +201,7 @@ window.onload = function () {
             },
         });
     };
-    // menuSilde();
+    menuSilde();
 
 
 
@@ -171,12 +213,15 @@ window.onload = function () {
         $('#lnb3_02, .consulting').addClass('on').siblings().removeClass('on');
     } else if (location.hash == "#lnb3_03") {
         $('#lnb3_03, .customizedEducation').addClass('on').siblings().removeClass('on');
+        customizedEdu();
     } else if (location.hash == "#lnb3_04") {
         $('#lnb3_04, .manage').addClass('on').siblings().removeClass('on');
     } else if (location.hash == "#lnb3_05") {
         $('#lnb3_05, .design').addClass('on').siblings().removeClass('on');
+        designSilde();
     } else if (location.hash == "#lnb3_06") {
         $('#lnb3_06, .menu').addClass('on').siblings().removeClass('on');
+        menuSilde();
     } else if (location.hash == "#lnb3_07") {
         $('#lnb3_07, .CS').addClass('on').siblings().removeClass('on');
     } else if (location.hash == "#lnb3_08") {
@@ -195,22 +240,15 @@ window.onload = function () {
             $(thisHashTrim).addClass('on').siblings().removeClass('on');
             $('#' + $(thisHashTrim).data('id')).addClass('on').siblings().removeClass('on'); // 그 id에 있는 데이터 아이디( data-id="Tab01", ...)로 아이디 찾아줌 ( id="Tab01")
 
+            customizedEdu();
+            designSilde();
+            menuSilde();
+            if (iw < 769) {
+                CSslide();
+            };
         });
     });
 
-    function CSslide() {
-        new Swiper(".CSslide", {
-            navigation: {
-                nextEl: ".Cc .swiper-button-next",
-                prevEl: ".Cc .swiper-button-prev",
-            },
-            observer: true,
-            observeParents: true,
-            //탭안의 스와이퍼 갱신
-            slidesPerView: 2,
-            spaceBetween: 10,
-        });
-    };
 
 
 
@@ -218,9 +256,24 @@ window.onload = function () {
     var iw = window.innerWidth;
     if (iw < 769) {
 
-        // CSslide();
+       
 
-
+        function CSslide() {
+            new Swiper(".CSslide", {
+                navigation: {
+                    nextEl: ".Cc .swiper-button-next",
+                    prevEl: ".Cc .swiper-button-prev",
+                },
+                observer: true,
+                observeParents: true,
+                //탭안의 스와이퍼 갱신
+                simulateTouch: false, // 드래깅 X
+                slidesPerView: 2,
+                spaceBetween: 10,
+            });
+        };
+    
+        CSslide();
 
 
         //클릭했을 때 nav 제거
--- arounz/js/index.js
+++ arounz/js/index.js
@@ -42,91 +42,10 @@ window.onload = function() {
 
 
 
-    // let CtabButtons = document.querySelectorAll('.CtabButton'); // 탭버튼 선언
-    // // 해당 클래스명을 가지고 있는 모든 dom을 유사배열로 가져온다. 진짜 배열이 아니어서 .forEach()같은 함수를 쓸 수 없다.
-    // // for문은 사용가능하나(인터넷에 돌아다니는 탭 자료들은 for문으로 제작됨)
-    // // .call()을 이용하여 .forEach()를 사용할 수 있다. Array.prototype.forEach.call()로 사용한다.
-
-    // Array.prototype.forEach.call(CtabButtons, (CeachButton) => { // 탭버튼에게 각각 적용한다
-    //     CeachButton.addEventListener('click', function() { //각 버튼을 클릭하면
-
-    //         let CtabBoxs = document.querySelectorAll('.CtabBox'); // 탭박스 선언
-    //         let CtabNum = this.getAttribute('data-num'); // 내가 누른 탭버튼에서 data-num 값 추출
-
-    //         for (var i = 0; i < CtabBoxs.length; i++) {
-    //             CtabBoxs[i].classList.remove("on");
-    //             CtabButtons[i].classList.remove("on");
-    //         }
-    //         CtabBoxs[CtabNum].classList.add("on"); // 내가 누른 탭버튼에서 data-num 값을 인덱스 번호/아이템 번호로 갖는 탭박스에 class on추가
-    //         // tabBoxs.item(tabNum).classList.add("on"); 
-    //         this.classList.add("on");
-    //         // if (!eachButton.classList.contains('on')) {
-    //         //     // .classList.contains 지정한 클래스가 있는지 확인한다.
-    //         //     // 내가 누른 탭버튼에서 class on이 없으면 추가해라
-    //         //     eachButton.classList.add("on");
-    //         // }
-    //     })
-    // })
 
 
 
-
-
-
-    // let EtabButtons = document.querySelectorAll('.EtabButton'); // 탭버튼 선언
-    // // 해당 클래스명을 가지고 있는 모든 dom을 유사배열로 가져온다. 진짜 배열이 아니어서 .forEach()같은 함수를 쓸 수 없다.
-    // // for문은 사용가능하나(인터넷에 돌아다니는 탭 자료들은 for문으로 제작됨)
-    // // .call()을 이용하여 .forEach()를 사용할 수 있다. Array.prototype.forEach.call()로 사용한다.
-
-    // Array.prototype.forEach.call(EtabButtons, (EeachButton) => { // 탭버튼에게 각각 적용한다
-    //     EeachButton.addEventListener('click', function() { //각 버튼을 클릭하면
-
-    //         let EtabBoxs = document.querySelectorAll('.EtabBox'); // 탭박스 선언
-    //         let EtabNum = this.getAttribute('data-num'); // 내가 누른 탭버튼에서 data-num 값 추출
-
-    //         for (var i = 0; i < EtabBoxs.length; i++) {
-    //             EtabBoxs[i].classList.remove("on");
-    //             EtabButtons[i].classList.remove("on");
-    //         }
-    //         EtabBoxs[EtabNum].classList.add("on"); // 내가 누른 탭버튼에서 data-num 값을 인덱스 번호/아이템 번호로 갖는 탭박스에 class on추가
-    //         // tabBoxs.item(tabNum).classList.add("on");
-    //         this.classList.add("on");
-    //         // if (!eachButton.classList.contains('on')) {
-    //         //     // .classList.contains 지정한 클래스가 있는지 확인한다.
-    //         //     // 내가 누른 탭버튼에서 class on이 없으면 추가해라
-    //         //     eachButton.classList.add("on");
-    //         // }
-    //     })
-    // })
-
-
-
-
-
-
-    // let tabButtons = document.querySelectorAll('.tabButton'); // 탭버튼 선언
-    //     // 해당 클래스명을 가지고 있는 모든 dom을 유사배열로 가져온다. 진짜 배열이 아니어서 .forEach()같은 함수를 쓸 수 없다.
-    //     // for문은 사용가능하나(인터넷에 돌아다니는 탭 자료들은 for문으로 제작됨)
-    //     // .call()을 이용하여 .forEach()를 사용할 수 있다. Array.prototype.forEach.call()로 사용한다.
-    //     Array.from(tabButtons).forEach((eachButton, index) => {
-    //         eachButton.addEventListener('click', function() {
-    //             let tabBoxs = document.querySelectorAll('.tabBox'); // 탭박스 선언
-    //             for (var i = 0; i < tabBoxs.length; i++) {
-    //                 tabBoxs[i].classList.remove("on");
-    //                 tabButtons[i].classList.remove("on");
-    //                 // 모든 버튼과 박스에서 on제거
-    //             }
-    //             tabBoxs[index].classList.add("on");
-    //             // 내가 누른 탭버튼과 인덱스번호가 같은 탭박스에 class on추가
-    //             this.classList.add("on");
-    //         })
-    //     })
-
-
-
-
-
-
+    //index 번호로  tab 만들기
     $('.CtabButton').click(function() {
         $(this).addClass('on').siblings().removeClass('on');
         $('.CtabBox').eq($(this).index()).addClass('on').siblings().removeClass('on');
@@ -150,7 +69,7 @@ window.onload = function() {
 
     function coffeeTabSilde() {
         var browser = navigator.userAgent;
-        if (browser.match(/Trident/)) {
+        if (browser.match(/Trident/)) { //ie 에서만 적용
             new Swiper(".coffeeWrap", {
                 navigation: {
                     nextEl: ".aa .swiper-button-next",
@@ -161,7 +80,7 @@ window.onload = function() {
                 observeParents: true,
                 //탭안의 스와이퍼 갱신
                 // touchRatio: 0,
-                simulateTouch: false,
+                simulateTouch: false, // 드래깅 X
                 slidesPerView: 5,
                 spaceBetween: 20,
                 breakpoints: {
